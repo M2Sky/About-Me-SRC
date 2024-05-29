654371663655f9d97fcae71b8664653716b913d0
M2Sky <122134945+M2Sky@users.noreply.github.com>
2024-05-29 19:38:16 -0400

    Update refer.bat

--- refer.bat
+++ refer.bat
@@ -1,22 +1,6 @@
 @echo off
-setlocal
-
-echo on
-
 echo Running build.bat...
 call "%~dp0build.bat"
-if %ERRORLEVEL% NEQ 0 (
-    echo Error: build.bat failed with exit code %ERRORLEVEL%.
-    goto end
-)
-
 echo Running startup.bat...
 call "%~dp0startup.bat"
-if %ERRORLEVEL% NEQ 0 (
-    echo Error: startup.bat failed with exit code %ERRORLEVEL%.
-    goto end
-)
-
-:end
-:: Keep the command prompt open
 cmd /k
